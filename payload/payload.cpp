c16e8c26cf5f5a321964c64162136f412cf6b8a2
Dave Cowden <dave.cowden@gmail.com>
2019-11-28 14:21:00 -0500

    adding other code

--- payload/payload.cpp
+++ payload/payload.cpp
@@ -1,67 +1,28 @@
-#include <Wire.h>
-#include "SSD1306Ascii.h"
-#include "SSD1306AsciiWire.h"
-#include "FastLED.h"
-#include "LedMeter.h"
+#include "Arduino.h"
 
-
-#define RST_PIN -1
-#define DEBUG 1
-
-#define I2C_ADDRESS 0x3C
-
-//pin definitions
-#define ROTARY_BTN_PIN 2
-#define LED_DATA_PIN 3
-#define BTN_FWD_1 4
-#define LEFT_MOTOR_ENABLE 5
-#define LEFT_MOTOR_A 6
-#define LEFT_MOTOR_B 7
-#define RIGHT_MOTOR_A 8
-#define RIGHT_MOTOR_B 9
-#define RIGHT_MOTOR_ENABLE 10
-#define BTN_FWD_2 11
-#define BTN_FWD_3 12
-#define BTN_BWD 13
 #define WIRE_SENSOR_RIGHT A0
 #define WIRE_SENSOR_LEFT A1
-//#define ENCODER_PIN_A A2
-//#define ENCODER_PIN_B A3
-//SDA=A5
-//SDC=A4
-
-
-
 
 //app constants
 #define FWD_SPEED_BASE_1X 200
 #define FWD_SPEED_BASE_2X 200
 #define BWD_SPEED_BASE -100
-
-#define P_GAIN 0.5
-#define D_GAIN 5.0
-
-
 #define NUM_ADC_SAMPLES 10
 #define MAX_SPEED 255
 #define MIN_SPEED -255
 #define MAX_SENSOR 775
 #define SENSOR_CLOSE 150
 #define SLOW_SPEED 100
-#define NUM_LEDS 13
-
-
-SSD1306AsciiWire oled;
-CRGB leds[NUM_LEDS];
-LedRange meterRanges [1] = {  { 0, 12 } } ;
-LedMeter mainMeter = LedMeter(leds,meterRanges,2,CRGB::Blue,CRGB::Black);
-
+#define BTN_FWD_1 4
+#define BTN_FWD_2 11
+#define BTN_FWD_3 12
+#define BTN_BWD 13
 
 struct Pose {
-  byte fwd_btn_1;
-  byte fwd_btn_2;
-  byte fwd_btn_3;
-  byte bwd_btn_1;
+  int fwd_btn_1;
+  int fwd_btn_2;
+  int fwd_btn_3;
+  int bwd_btn_1;
   int left_sensor;
   int right_sensor;
 };
@@ -77,62 +38,8 @@ struct GameData {
 
 GameData gameData = {0};
 
-void setup() {
-  Serial.begin(38400);
-  setupOLED();
-  setupLEDs();
-  setupPins();
-}
-
-void setupLEDs(){
-  mainMeter.setMaxValue(100);
-  FastLED.addLeds<NEOPIXEL, LED_DATA_PIN>(leds, NUM_LEDS);
-}
-
-void setupOLED(){
-  Wire.begin();
-  Wire.setClock(400000L);  
-  oled.begin(&Adafruit128x64, I2C_ADDRESS);
-
-  //invert display
-  oled.displayRemap(false);
-  oled.setFont(Adafruit5x7);
-  oled.clear();
-  oled.println("BattlePoint v1.0"); 
-  oled.println("Starting..."); 
-  
-}
-
 void startGame(){
   gameData.startTime = millis();
-}
-
-void setupPins(){
-  pinMode(LEFT_MOTOR_ENABLE, OUTPUT);
-  pinMode(LEFT_MOTOR_A, OUTPUT);
-  pinMode(LEFT_MOTOR_B, OUTPUT);
-  pinMode(RIGHT_MOTOR_A, OUTPUT);
-  pinMode(RIGHT_MOTOR_B, OUTPUT);
-  pinMode(RIGHT_MOTOR_ENABLE, OUTPUT);
-  pinMode(WIRE_SENSOR_LEFT,INPUT);
-  pinMode(WIRE_SENSOR_RIGHT,INPUT);
-  pinMode(BTN_FWD_1,INPUT_PULLUP);
-  pinMode(BTN_FWD_2,INPUT_PULLUP);
-  pinMode(BTN_FWD_3,INPUT_PULLUP);
-  pinMode(BTN_BWD,INPUT_PULLUP);
-  
-}
-
-
-void loop() {
-  Pose p = readPose();
-  PayloadCommand pc = computePayloadCommand(p);
-  handleCommand(pc);
-  #if DEBUG
-  updateSerial(p,pc);
-  #endif
-  updateDisplay(p,pc);
-  updateLEDs(p,pc);
 }
 
 Pose readPose(){
@@ -150,73 +57,6 @@ int readButton(int buttonPin){
   int i = digitalRead(buttonPin);
   return i == 0;
 }
-
-void updateLEDs(Pose p, PayloadCommand pc){
-  mainMeter.update();
-  FastLED.show();
-}
-
-void updateDisplay(Pose p, PayloadCommand pc){
-  int gameTimeSec = (millis() - gameData.startTime)/1000;
-  char SPACE = ' ';
-  oled.setCursor(0,1);
-  oled.print("Time: ");
-  oled.print(gameTimeSec);
-  oled.print(" s");
-  oled.clearToEOL();
-  
-  oled.setCursor(0,2);
-  oled.print("BTNS:");
-  oled.print(SPACE);
-  oled.print(p.fwd_btn_1);
-  oled.print(SPACE);
-  oled.print(p.fwd_btn_2);
-  oled.print(SPACE);
-  oled.print(p.fwd_btn_3);
-  oled.print(SPACE);
-  oled.print(p.bwd_btn_1);
-  oled.clearToEOL();
-  oled.setCursor(0,3);
-  oled.print("SENS: L:");
-  oled.print(p.left_sensor);
-  oled.print(" R:");   
-  oled.print(p.right_sensor);
-  oled.print(SPACE);
-  oled.clearToEOL();
-  oled.setCursor(0,4);
-  oled.print("MTR: L ");  
-  oled.print(pc.leftSpeed);
-  oled.print(" R: ");
-  oled.print(pc.rightSpeed);
-  oled.clearToEOL();
-}
-
-void updateSerial(Pose p, PayloadCommand pc){
-   char SPACE = ' ';
-   Serial.print("B:");
-   Serial.print(SPACE);
-   Serial.print(p.fwd_btn_1);
-   Serial.print(SPACE);
-   Serial.print(p.fwd_btn_2);
-   Serial.print(SPACE);
-   Serial.print(p.fwd_btn_3);
-   Serial.print(SPACE);
-   Serial.print(p.bwd_btn_1);
-   Serial.print(SPACE);
-   Serial.print("L:");
-   Serial.print(p.left_sensor);
-   Serial.print(SPACE);
-   Serial.print("R:");   
-   Serial.print(p.right_sensor);
-   Serial.print(SPACE);     
-   Serial.print("LM:");
-   Serial.print(pc.leftSpeed);
-   Serial.print(SPACE);   
-   Serial.print("RM:");
-   Serial.print(pc.rightSpeed);
-   Serial.println(SPACE);     
-}
-
 int lastError = 0;
 
 PayloadCommand computePayloadCommand(Pose p ){
@@ -239,21 +79,15 @@ PayloadCommand computePayloadCommand(Pose p ){
     }
     pc.leftSpeed = constrain(nominal_speed - correction,MIN_SPEED,MAX_SPEED);
     pc.rightSpeed = constrain(nominal_speed + correction,MIN_SPEED,MAX_SPEED);
-
-    
   }
-  
-  
   return pc;
 }
 
 void handleCommand(PayloadCommand mc ){
-  setRightMotorSpeed(mc.rightSpeed );
-  setLeftMotorSpeed(mc.leftSpeed);
+
 }
 
 int computeNominalSpeed(Pose p ){
-
   int num_fwd = num_fwd_pressed(p);
   if ( num_fwd == 3 ){
     return MAX_SPEED ;
@@ -274,27 +108,6 @@ int computeNominalSpeed(Pose p ){
 
 int num_fwd_pressed ( Pose p ){
   return p.fwd_btn_1 + p.fwd_btn_2 + p.fwd_btn_3 - p.bwd_btn_1;
-}
-
-void setRightMotorSpeed( int velocity){
-  setMotorSpeed( RIGHT_MOTOR_A, RIGHT_MOTOR_B, RIGHT_MOTOR_ENABLE, velocity );
-}
-
-void setLeftMotorSpeed( int velocity){
-  setMotorSpeed( LEFT_MOTOR_A, LEFT_MOTOR_B, LEFT_MOTOR_ENABLE, velocity );
-}
-
-void setMotorSpeed ( int pinA, int pinB, int pinEnable, int velocity ){
-  int speed = abs(velocity);
-  analogWrite(pinEnable, speed );
-  if ( velocity > 0 ){
-    digitalWrite(pinA, HIGH);
-    digitalWrite(pinB, LOW);     
-  }
-  else{
-    digitalWrite(pinA, LOW);
-    digitalWrite(pinB, HIGH);    
-  }
 }
 
 int readADCPinPeak(int pin, int samples ){
--- payload/payload.ino
+++ payload/payload.ino
@@ -2,6 +2,8 @@
 #include <FastLED.h>
 #include <ESP32Encoder.h>
 #include "LedMeter.h"
+#include "SSD1306Ascii.h"
+#include "SSD1306AsciiWire.h"
 
 #define ROTARY_ENCODER_A_PIN 14
 #define ROTARY_ENCODER_B_PIN 13
@@ -26,7 +28,35 @@ void setup() {
   timer1Meter.setMaxValue(13);
 }
 
+//void setupOLED(){
+//  Wire.begin();
+//  Wire.setClock(400000L);  
+//  oled.begin(&Adafruit128x64, I2C_ADDRESS);
+//
+//  //invert display
+//  oled.displayRemap(false);
+//  oled.setFont(Adafruit5x7);
+//  oled.clear();
+//  oled.println("BattlePoint v1.0"); 
+//  oled.println("Starting..."); 
+//  
+//}
+
 void loop() {
+
+//void updateLEDs(Pose p, PayloadCommand pc){
+//  mainMeter.update();
+//  FastLED.show();
+//}
+
+//  Pose p = readPose();
+//  PayloadCommand pc = computePayloadCommand(p);
+//  handleCommand(pc);
+//  #if DEBUG
+//  updateSerial(p,pc);
+//  #endif
+//  updateDisplay(p,pc);
+//  updateLEDs(p,pc);
   
   encoderValue = encoder.getCount();
   Serial.print("Val= "); Serial.println(encoderValue);
@@ -34,4 +64,65 @@ void loop() {
   FastLED.show();
   //FastLED.delay(200);
   delay(100);
+}
+
+void updateDisplay(Pose p, PayloadCommand pc){
+  int gameTimeSec = (millis() - gameData.startTime)/1000;
+  char SPACE = ' ';
+  oled.setCursor(0,1);
+  oled.print("Time: ");
+  oled.print(gameTimeSec);
+  oled.print(" s");
+  oled.clearToEOL();
+  
+  oled.setCursor(0,2);
+  oled.print("BTNS:");
+  oled.print(SPACE);
+  oled.print(p.fwd_btn_1);
+  oled.print(SPACE);
+  oled.print(p.fwd_btn_2);
+  oled.print(SPACE);
+  oled.print(p.fwd_btn_3);
+  oled.print(SPACE);
+  oled.print(p.bwd_btn_1);
+  oled.clearToEOL();
+  oled.setCursor(0,3);
+  oled.print("SENS: L:");
+  oled.print(p.left_sensor);
+  oled.print(" R:");   
+  oled.print(p.right_sensor);
+  oled.print(SPACE);
+  oled.clearToEOL();
+  oled.setCursor(0,4);
+  oled.print("MTR: L ");  
+  oled.print(pc.leftSpeed);
+  oled.print(" R: ");
+  oled.print(pc.rightSpeed);
+  oled.clearToEOL();
+}
+
+void updateSerial(Pose p, PayloadCommand pc){
+   char SPACE = ' ';
+   Serial.print("B:");
+   Serial.print(SPACE);
+   Serial.print(p.fwd_btn_1);
+   Serial.print(SPACE);
+   Serial.print(p.fwd_btn_2);
+   Serial.print(SPACE);
+   Serial.print(p.fwd_btn_3);
+   Serial.print(SPACE);
+   Serial.print(p.bwd_btn_1);
+   Serial.print(SPACE);
+   Serial.print("L:");
+   Serial.print(p.left_sensor);
+   Serial.print(SPACE);
+   Serial.print("R:");   
+   Serial.print(p.right_sensor);
+   Serial.print(SPACE);     
+   Serial.print("LM:");
+   Serial.print(pc.leftSpeed);
+   Serial.print(SPACE);   
+   Serial.print("RM:");
+   Serial.print(pc.rightSpeed);
+   Serial.println(SPACE);     
 }
